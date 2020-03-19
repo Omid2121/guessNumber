9ba5e9aa7749e2d421b935a1d20ce58e4b2affe8
Omid2121 <Omid_Ahangi@github.com>
2020-03-19 14:18:45 -0700

    første del
    3 chancer for at gætte det tilfældige antal.
    Næste skridt :Win Streak, Highscore og Gem Highscore

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessNumber
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 11);
+            int input;
+            int numberOfGuesses = 3;
+            bool gameOver = false;
+
+            Console.WriteLine("Guess number between 1 & 10 : ");
+             
+
+            while (gameOver == false)
+            {
+                input = Convert.ToInt32(Console.ReadLine());
+                numberOfGuesses--;
+
+                if (input != randomNumber && numberOfGuesses == 0)
+                {
+                    Console.WriteLine("Game over for you!");
+                    gameOver = true;
+                }
+                else if (input == randomNumber)
+                {
+                    Console.WriteLine("You win!!");
+                    gameOver = true;
+                }
+                else if (input > randomNumber)
+                    Console.WriteLine("Too heigh, try again.");
+
+                else if (input < randomNumber)
+                    Console.WriteLine("Too low, try again. ");
+       
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
