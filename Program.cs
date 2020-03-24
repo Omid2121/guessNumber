78684ee378f2d1aa00d85e8792dcf283c47b1b76
Omid2121 <Omid_Ahangi@github.com>
2020-03-24 13:28:58 -0700

    genstarte spil

--- Program.cs
+++ Program.cs
@@ -33,7 +33,17 @@ namespace guessNumber
                 else if (input == randomNumber)
                 {
                     Console.WriteLine("You win!!");
-                    gameOver = true;
+                    
+                    Console.WriteLine("Do you want to play again?(yes or no)");
+                    string restart = Console.ReadLine();
+                    if (restart =="no")
+                    {
+                       gameOver = true;
+                    }
+                    else
+                    {
+                        gameOver = false;
+                    }
                 }
                 else if (input > randomNumber)
                     Console.WriteLine("Too heigh, try again.");
@@ -42,6 +52,10 @@ namespace guessNumber
                     Console.WriteLine("Too low, try again. ");
        
             }
+            //for (int i = 0; i < length; i++)
+            //{
+
+            //}
             Console.ReadLine();
 
         }
